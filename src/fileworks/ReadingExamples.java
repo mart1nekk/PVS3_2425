4b6a984f9b3f8b77307348dd18e24784c400af7f
bohac <honza.boh00@gmail.com>
2024-12-05 10:34:55 +0100

    Files - reading part 1

---
+++ src/fileworks/ReadingExamples.java
@@ -0,0 +1,44 @@
+package fileworks;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ReadingExamples {
+
+    public static void main(String[] args) throws IOException {
+        File file = new File("data\\countries.txt");
+        if (file.exists() && file.isFile()){
+            DataImport di = new DataImport(file.getPath());
+            while (di.hasNext()){
+                System.out.println(di.readLine());
+            }
+            di.finishImport();
+            System.out.println();
+        }
+//        Scanner sc = new Scanner("hello,world,its,beautiful,day");
+//        sc.useDelimiter(",");
+//        while (sc.hasNext()){
+//            System.out.println(sc.next());
+//        }
+        Scanner sc = new Scanner(file);
+        sc.useLocale(Locale.US);
+        sc.useDelimiter(";");
+        while (sc.hasNext()){
+            System.out.println(sc.next());
+        }
+        sc.close();
+
+        FileReader reader = new FileReader(file);
+        int input;
+        while ((input = reader.read()) != -1){
+            System.out.print((char) input);
+        }
+        reader.close();
+
+    }
+
+}
