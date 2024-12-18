14418215a464604c12286d7614795a6fc199e132
bohac <honza.boh00@gmail.com>
2024-12-18 09:34:41 +0100

    BloodBank setup

--- src/fileworks/BloodBank.java
+++ src/fileworks/BloodBank.java
@@ -1,5 +1,11 @@
 package fileworks;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
 public class BloodBank {
 
 
@@ -7,6 +13,40 @@ public class BloodBank {
         for (int i = 0; i < times; i++) {
             // TODO: 18.12.2024 Nahodna dvojice se pokusi darovat
         }
+    }
+
+
+    static BloodType parseBlood(String bloodType){
+        switch (bloodType){
+            case "A+": return BloodType.A_POSITIVE;
+            case "B+": return BloodType.B_POSITIVE;
+            case "AB+": return BloodType.AB_POSITIVE;
+            case "0+": return BloodType.O_POSITIVE;
+            case "A-": return BloodType.A_NEGATIVE;
+            case "B-": return BloodType.B_NEGATIVE;
+            case "AB-": return BloodType.AB_NEGATIVE;
+            case "0-": return BloodType.O_NEGATIVE;
+            default: return BloodType.AB_POSITIVE;
+        }
+    }
+
+    ArrayList<BloodDonor> getDonors(String filePath) throws IOException{
+        ArrayList<BloodDonor> donors = new ArrayList<>();
+        List<String> lines = Files.readAllLines(Paths.get(filePath));
+        lines.remove(0);//prvni radek jsou nazvy sloupcu
+        String[] params;
+        BloodDonor donor;
+        for (String line : lines){
+            params = line.split(",");
+            donor = new BloodDonor(
+                    params[0],
+                    Integer.parseInt(params[1]),
+                    params[2],
+                    BloodType.valueOf("TBD")
+            );
+            donors.add(donor);
+        }
+        return donors;
     }
 
     public static void main(String[] args) {
